f9aeb8c7b31e9ec4dadaaddd71a67b5cc6ac1a59
Informatica <informatica@alumno.local>
2026-09-17 15:52:57 -0600

    Se agrego el crud de user

---
+++ src/main/java/com/jgunzalesindustries/banco/sahur/config/Credentials.java
@@ -0,0 +1,12 @@
+
+package main.java.com.jgunzalesindustries.banco.sahur.config;
+
+
+public class Credentials {
+    public static final String URL_DB = System.getenv("DB_URL");
+    public static final String USER_DB = System.getenv("USER_DB");
+    public static final String PASS_DB = System.getenv("PASS_DB");
+    
+}
+
+
--- src/main/java/com/jgunzalesindustries/banco/sahur/config/DataBaseConnection.java
+++ src/main/java/com/jgunzalesindustries/banco/sahur/config/DataBaseConnection.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main.java.com.jgunzalesindustries.banco.sahur.config;
 
-/**
- *
- * @author informatica
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBaseConnection {
+    //atributos
+    private static Connection connection;
     
+    /*constructor
+    El constructor tiene que ser privado, porque NO permite que la clase sea instanciada
+    */
+    
+    private DataBaseConnection(){
+    
+    };
+    
+    //método 
+    public static Connection getDataBaseConnection() throws SQLException{
+        if(connection == null || connection.isClosed()){
+            connection = DriverManager.getConnection(Credentials.URL_DB, Credentials.USER_DB, Credentials.PASS_DB);
+        }
+        return connection; 
+    }
 }
